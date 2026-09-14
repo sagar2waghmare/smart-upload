489cf371832c293529e7f78b38b211235673b494
sagar2waghmare <122687260+sagar2waghmare@users.noreply.github.com>
2026-09-14 21:44:28 +0530

    Allow Google Drive thumbnail hosts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "image.tmdb.org" },
+      { protocol: "https", hostname: "*.googleusercontent.com" },
       { protocol: "https", hostname: "**.googleapis.com" },
       { protocol: "https", hostname: "**.amazonaws.com" },
     ],
