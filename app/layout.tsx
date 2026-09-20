15dcef1b9644c3f1d2e7e6b144c74bac9d821cd3
sagar2waghmare <122687260+sagar2waghmare@users.noreply.github.com>
2026-09-20 18:38:02 +0530

    ui: load premium visual enhancement layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./abyss-theme.css";
 import "./cinematic-polish.css";
 import "./ui-motion.css";
 import "./player-polish.css";
+import "./premium-ui.css";
 import { Header } from "../components/Header";
 import { AuthProvider } from "../components/AuthProvider";
 import { DetailsProvider } from "../components/DetailsProvider";
