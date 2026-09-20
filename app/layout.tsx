06b2c779283a155cf26fd939b4c8d10b218d9a05
sagar2waghmare <122687260+sagar2waghmare@users.noreply.github.com>
2026-09-20 21:28:21 +0530

    Load Smart Upload Video.js styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./cinematic-polish.css";
 import "./ui-motion.css";
 import "./player-polish.css";
 import "./premium-ui.css";
+import "./videojs-smart.css";
 import { Header } from "../components/Header";
 import { AuthProvider } from "../components/AuthProvider";
 import { DetailsProvider } from "../components/DetailsProvider";
