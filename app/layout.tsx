6485f72d2d17bf48b372da8545b1aed180d3b758
sagar2waghmare <122687260+sagar2waghmare@users.noreply.github.com>
2026-09-21 16:10:34 +0530

    Load final UI polish after existing theme layers

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import "./ui-motion.css";
 import "./player-polish.css";
 import "./premium-ui.css";
 import "./player-mobile-fixes.css";
+import "./final-ui-polish.css";
 import { Header } from "../components/Header";
 import { AuthProvider } from "../components/AuthProvider";
 import { DetailsProvider } from "../components/DetailsProvider";
