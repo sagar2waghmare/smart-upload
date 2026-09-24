7883f539ee08ed83016a0b364a2a03c9d66b9930
sagar2waghmare <122687260+sagar2waghmare@users.noreply.github.com>
2026-09-24 20:41:18 +0530

    Load metadata enrichment only on library pages

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Rail } from "../components/Rail";
 import { SignInPrompt } from "../components/SignInPrompt";
 import { LoginScreen } from "../components/LoginScreen";
 import { ContinueWatchingRail } from "../components/ContinueWatchingRail";
+import { LibraryEnrichmentProvider } from "../components/LibraryEnrichmentProvider";
 import { getLibrary } from "../lib/library-service";
 import { authIntended, requireSession } from "../lib/auth";
 
@@ -16,24 +17,26 @@ export default async function Home({
     return <LoginScreen />;
   }
   const showSignInPrompt = signin === "1";
-  const { items } = await getLibrary();
+  const { items, enrichmentTargets } = await getLibrary();
   const heroItems = items.slice(0, 5);
   const movies = items.filter((item) => item.kind === "movie");
   const tvShows = items.filter((item) => item.kind === "series");
   const anime = items.filter((item) => item.kind === "anime");
 
   return (
-    <main className="home-main">
-      <Hero items={heroItems} />
-      <div className="page home-content">
-        <SignInPrompt prompt={showSignInPrompt} />
-        <ContinueWatchingRail />
-        <Rail title="Movies" items={movies.slice(0, 12)} seeAll="/browse/movie" />
-        <Rail title="TV Shows" items={tvShows.slice(0, 12)} seeAll="/browse/series" />
-        {anime.length > 0 && (
-          <Rail title="Anime" items={anime.slice(0, 12)} seeAll="/browse/anime" />
-        )}
-      </div>
-    </main>
+    <LibraryEnrichmentProvider targets={enrichmentTargets}>
+      <main className="home-main">
+        <Hero items={heroItems} />
+        <div className="page home-content">
+          <SignInPrompt prompt={showSignInPrompt} />
+          <ContinueWatchingRail />
+          <Rail title="Movies" items={movies.slice(0, 12)} seeAll="/browse/movie" />
+          <Rail title="TV Shows" items={tvShows.slice(0, 12)} seeAll="/browse/series" />
+          {anime.length > 0 && (
+            <Rail title="Anime" items={anime.slice(0, 12)} seeAll="/browse/anime" />
+          )}
+        </div>
+      </main>
+    </LibraryEnrichmentProvider>
   );
 }
